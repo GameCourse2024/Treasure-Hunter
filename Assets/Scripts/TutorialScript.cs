b92cd3d6329d7c8e6d933aa382b08bdf36116d3a
Eli <eligreenblatt9@gmail.com>
2024-03-12 17:36:54 +0200

    Updated Tutorial

--- Assets/Scripts/TutorialScript.cs
+++ Assets/Scripts/TutorialScript.cs
@@ -20,10 +20,16 @@ public class TutorialScript : MonoBehaviour
 
     [SerializeField]
     private TextMeshProUGUI explanationText;
-    private int timesClicked = 0;
+    private int timesClicked = 1;
+
+    [SerializeField] 
+    private Image explanationImage;
+
 
     [SerializeField]
     private string[] explanations = new string[5];
+    [SerializeField]
+    private Sprite[] images = new Sprite[5];
 
     public void PlayClicked()
     {
@@ -40,11 +46,30 @@ public class TutorialScript : MonoBehaviour
         nextButton.gameObject.SetActive(true);
         playButton.gameObject.SetActive(false);
         tutorialButton.gameObject.SetActive(false);
+        explanationText.gameObject.SetActive(true);
+        explanationImage.gameObject.SetActive(true);
+
     }
 
     public void NextClicked()
     {
+        if(timesClicked == explanations.Length)
+        {
+            explanationImage.sprite = images[0];
+            explanationText.text = explanations[0];
+            explanationText.gameObject.SetActive(false);
+            explanationImage.gameObject.SetActive(false);
+            nextButton.gameObject.SetActive(false);
+            playButton.gameObject.SetActive(true);
+            tutorialButton.gameObject.SetActive(true);
+            timesClicked = 0;
+            return;
+        }
+        Debug.Log("Next Clicked");
         explanationText.gameObject.SetActive(true);
+        explanationImage.gameObject.SetActive(true);
+        explanationText.text = explanations[timesClicked];
+        explanationImage.sprite = images[timesClicked];
         timesClicked++;
     }
 }
