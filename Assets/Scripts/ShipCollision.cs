a712420bb73c22f00f7864e73df102bc57f4d948
itamarc101 <itamarc101@gmail.com>
2024-03-15 16:50:04 +0200

    fixes sound ship

--- Assets/Scripts/ShipCollision.cs
+++ Assets/Scripts/ShipCollision.cs
@@ -1,33 +1,32 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-
 public class ShipCollision : MonoBehaviour
 {
-    [SerializeField] private string soundHit;
+    [SerializeField] private float timer = 0.8f;
 
     void OnCollisionEnter(Collision collision)
     {
         // Check if the collision object has a tag
-        if (collision.gameObject.tag == "Player")
+        if (collision.gameObject.CompareTag("Player"))
         {
-            AudioManagerGamePlay.Instance.Play(soundHit);
+            AudioManagerGamePlay.Instance.Play("ShipCrash");
 
             // Print the tag of the object that hit the ship
             Debug.Log("Hit by: " + collision.gameObject.tag);
-            ReloadScene();
+            StartCoroutine(ReloadSceneAfterDelay());
         }
     }
 
-    void ReloadScene()
+    IEnumerator ReloadSceneAfterDelay()
     {
+        yield return new WaitForSeconds(timer);
+
         // Get the current scene index
         int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
 
         // Reload the current scene
         SceneManager.LoadScene(currentSceneIndex);
     }
-
 }
